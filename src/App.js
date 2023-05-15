db30d527a2160bd1b5aaef35d57084477e3e2493
Akshay Kumar <akshaykumar2k1@gmail.com>
2023-05-15 23:58:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import 'chart.js';
+
+import { Chart, CategoryScale, LinearScale, BarController, BarElement } from 'chart.js';
+
+Chart.register(CategoryScale, LinearScale, BarController, BarElement);
+
+function App() {
+  const [text, setText] = useState(null);
+  const [histogramData, setHistogramData] = useState(null);
+
+  const handleClick = async () => {
+    const response = await fetch('https://www.terriblytinytales.com/test.txt');
+    const data = await response.text();
+    const frequency = getFrequency(data);
+    setText(frequency);
+    setHistogramData(getHistogramData(frequency));
+  };
+
+  const getFrequency = (text) => {
+    const words = text
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9 ]/g, "")
+      .split(" ");
+    const frequency = {};
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (frequency[word]) {
+        frequency[word]++;
+      } else {
+        frequency[word] = 1;
+      }
+    }
+    return frequency;
+  };
+
+  const getTopWords = (frequency) => {
+    const sorted = Object.entries(frequency).sort((a, b) => b[1] - a[1]);
+    return sorted.slice(0, 20).map(([word, count]) => word);
+  };
+  
+  const getHistogramData = (frequency) => {
+    const topWords = getTopWords(frequency);
+    const data = {
+      labels: topWords,
+      datasets: [
+        {
+          label: 'Word Frequency',
+          backgroundColor: 'rgba(0, 128, 0, 0.2))',
+    borderColor: 'rgba(0, 128, 0, 1)',
+          borderWidth: 1,
+          data: topWords.map((word) => frequency[word]),
+        },
+      ],
+    };
+    return data;
+  };
+
+  const exportCSV = () => {
+    console.log(histogramData);
+    if (histogramData) {
+      let csvContent =
+        "data:text/csv;charset=utf-8," +
+        histogramData.labels.join(",") +
+        "\n" +
+        histogramData.datasets[0].data.join(",");
+      const encodedUri = encodeURI(csvContent);
+      const link = document.createElement("a");
+      link.setAttribute("href", encodedUri);
+      link.setAttribute("download", "histogram.csv");
+      document.body.appendChild(link);
+      link.click();
+    }
+  };
+
+  return (
+    <div>
+      {!text && <button onClick={handleClick}>Submit</button>}
+      {text && (
+        <div>
+          <h2>Frequency Distribution Chart of  Histogram</h2>
+          <Bar data={histogramData} />
+          <button onClick={exportCSV}>Export</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
